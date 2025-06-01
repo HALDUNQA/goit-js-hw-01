d26225755e0f555f7262a8d2744d87c832fca149
Haldun <cetinkanat@gmail.com>
2025-06-01 15:48:00 -0400

    task2 update

--- js/task-3.js
+++ js/task-3.js
@@ -12,11 +12,9 @@
 
  }   
 
- const content= "50px";
- const padding= "8.5px";
- const border= "4px";
-
- console.log(getElementWidth(content, padding, border));
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
 
 
 
